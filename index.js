605092e9a365e4d7fedfdc2e7e2141c80bd0711f
Josh0011 <joshuasharma4@gmail.com>
2024-12-13 21:38:31 -0500

    finale

--- index.js
+++ index.js
@@ -9,28 +9,39 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
 
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
-mongoose.connect(CONNECTION_STRING);
+dotenv.config();
+mongoose
+    .connect(process.env.MONGO_CONNECTION_STRING)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+
+
+//const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING // || "mongodb://127.0.0.1:27017/kanbas"
+//mongoose.connect(CONNECTION_STRING);
 
 
 const app = express()
 
 app.use(cors({
                  credentials: true,
-                 origin: process.env.NETLIFY_URL, //|| "http://localhost:3000",
+                 origin: process.env.NETLIFY_URL || "http://localhost:3000",
              }
 ));
+
+
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kanbas",
     resave: false,
     saveUninitialized: false,
 };
+
+
 if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
         sameSite: "none",
-        secure: true,
         domain: process.env.NODE_SERVER_DOMAIN,
     };
 }
